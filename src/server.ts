3c9c74acdc3335d8f62c5df244a0643c2aabd90f
Arpit Dubey <arpit@gamezop.co>
2020-01-25 14:55:54 +0000

    refactor(server): working stuff

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,12 @@ import express from "express";
 import http from "http";
 import bodyParser from "body-parser";
 import socketIO from "socket.io";
+import twilio from "twilio";
+import { TWILIO_SID } from "./constants";
+import { TWILIO_AUTH_TOKEN } from "./constants";
 const app = express();
 const server = http.createServer(app);
+const twilioClient = twilio(TWILIO_SID, TWILIO_AUTH_TOKEN);
 
 app.use(bodyParser.json());
 
@@ -12,11 +16,19 @@ const io = socketIO(server);
 
 let users: { socketId: string; userName: string }[] = [];
 
+const getIceServerConfig = async () => {
+	const iceServerConfig = await twilioClient.tokens.create();
+	return iceServerConfig;
+};
 const createUser = (socket: SocketIO.Socket) => {
 	return {
 		userName: socket.handshake.query.userName,
 		socketId: socket.id,
 	};
+};
+
+const getUser = userName => {
+	return users.filter(user => user.userName === userName)[0];
 };
 
 io.on("connection", function(socket: SocketIO.Socket) {
@@ -29,10 +41,31 @@ io.on("connection", function(socket: SocketIO.Socket) {
 		console.log("user disconnected");
 		console.log(users);
 	});
+
+	socket.on("connectCall", function(data) {
+		const callTo = data.callTo;
+		const userToCall = getUser(callTo);
+		io.to(userToCall.socketId).emit("incomingCall", data);
+	});
+
+	socket.on("answerIncomingCall", function(data) {
+		const answerTo = data.answerTo;
+		const userToSendTheAnswer = getUser(answerTo);
+		io.to(userToSendTheAnswer.socketId).emit("callAccepted", data);
+	});
+
+	socket.on("newIceCandidate", data => {
+		const sendToUser = getUser(data.sendTo);
+		io.to(sendToUser.socketId).emit("incomingIceCandidate", data);
+	});
 });
 
 app.get("/", function(req, res) {
 	res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/breakTheIce", async function(req, res) {
+	res.send({ config: await getIceServerConfig() });
 });
 
 app.get("/connect", function(req, res) {

2e6e6a443935be08a2682e114979f16deb82f367
Arpit Dubey <arpit1294@gmail.com>
2020-02-27 17:57:58 +0000

    refactor(some fixes): gp

--- src/server.ts
+++ src/server.ts
@@ -8,14 +8,11 @@ import { TWILIO_SID, TWILIO_AUTH_TOKEN } from "./constants";
 const app = express();
 const server = http.createServer(app);
 const twilioClient = twilio(TWILIO_SID, TWILIO_AUTH_TOKEN);
-
 app.use(bodyParser.json());
 app.use(cors());
 server.listen(process.env.PORT || 3000);
 const io = socketIO(server);
-
 let users: { socketId: string; userName: string; link?: string }[] = [];
-
 const getIceServerConfig = async () => {
 	const iceServerConfig = await twilioClient.tokens.create();
 	return iceServerConfig;
@@ -26,15 +23,12 @@ const createUser = (socket: SocketIO.Socket) => {
 		socketId: socket.id,
 	};
 };
-
 const getUser = userName => {
 	return users.filter(user => user.userName === userName)[0];
 };
-
 const getUserBySocket = id => {
 	return users.filter(user => user.socketId === id)[0];
 };
-
 setInterval(() => {
 	try {
 		for (let user of users) {
@@ -44,12 +38,10 @@ setInterval(() => {
 		console.log(err);
 	}
 }, 2000);
-
 io.on("connection", function(socket: SocketIO.Socket) {
 	console.log("connecting user");
 	users.push(createUser(socket));
 	console.log(users);
-
 	socket.on("peersConnected", function(data) {
 		users = users.map(user => {
 			if (data.caller === user.userName) {
@@ -75,7 +67,6 @@ io.on("connection", function(socket: SocketIO.Socket) {
 		users = users.filter(user => user.socketId !== socket.id);
 		console.log("user got disconnected");
 	});
-
 	socket.on("connectCall", function(data) {
 		const callTo = data.callTo;
 		const userToCall = getUser(callTo);
@@ -85,7 +76,6 @@ io.on("connection", function(socket: SocketIO.Socket) {
 			io.to(socket.id).emit("userOut", {});
 		}
 	});
-
 	socket.on("answerIncomingCall", function(data) {
 		const answerTo = data.answerTo;
 		const userToSendTheAnswer = getUser(answerTo);
@@ -95,7 +85,6 @@ io.on("connection", function(socket: SocketIO.Socket) {
 			io.to(socket.id).emit("userOut", {});
 		}
 	});
-
 	socket.on("newIceCandidate", data => {
 		const sendToUser = getUser(data.sendTo);
 		if (sendToUser) {
@@ -105,11 +94,9 @@ io.on("connection", function(socket: SocketIO.Socket) {
 		}
 	});
 });
-
 app.get("/", function(_, res) {
 	res.sendFile(__dirname + "/index.html");
 });
-
 app.get("/checkAvailability", function(req, res) {
 	const userName = req.query.userName;
 	const user = getUser(userName);
@@ -121,11 +108,9 @@ app.get("/checkAvailability", function(req, res) {
 		return res.send({ status: true });
 	}
 });
-
 app.get("/breakTheIce", async function(_, res) {
 	res.send({ config: await getIceServerConfig() });
 });
-
 app.get("/connect", function(req, res) {
 	if (!users.some(user => user.userName === req.query.userName)) {
 		return res.send({ status: true });
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+
+module.exports = {
+	mode: "production",
+	entry: {
+		sdk: "./src/sdk.ts",
+	},
+	optimization: {
+		minimizer: [
+			new UglifyJsPlugin({
+				uglifyOptions: {
+					output: {
+						comments: false,
+					},
+				},
+			}),
+		],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts?$/,
+				use: ["babel-loader", "ts-loader"],
+				exclude: /node_modules/,
+			},
+		],
+	},
+	resolve: {
+		extensions: [".ts", ".js"],
+	},
+	output: {
+		path: path.resolve(__dirname, "build"),
+	},
+};

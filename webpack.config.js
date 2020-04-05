69e530352a124d4b70ffe509bf5a3e9417f2141d
Arpit Dubey <arpit1294@gmail.com>
2020-04-05 05:52:22 +0000

    feat(file copy): copy file

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,14 @@
 const path = require("path");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
+	plugins: [
+		new CopyPlugin([
+			{ from: "src/sdk.js", to: "build/sdk.js" },
+			{ from: "src/app.js", to: "build/app.js" },
+		]),
+	],
 	mode: "production",
 	entry: {
 		sdk: "./src/sdk.ts",
